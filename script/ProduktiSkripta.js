d757f17c834449c6a17141c2d5b02fe02ab85941
kerimAbsIt <jolinecaroline@gmail.com>
2023-10-31 12:54:39 +0100

    Importovani prek modula sta znam

---
+++ script/ProduktiSkripta.js
@@ -0,0 +1,124 @@
+export let Produkti=[
+    {
+        image:"/Ikone proizvoda lol/freebudsPro3.jpg",
+        ime:'Huawei Freebuds Pro 3' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:87
+        },
+        Cijena: 14990,
+        
+
+    },
+    {
+        image:"/Ikone proizvoda lol/mate50pro.jpg",
+        ime:'Huawei Mate 50 Pro' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:24
+        },
+        Cijena: 1099000,
+
+    },
+    {
+        image:'/Ikone proizvoda lol/MatePad11.5.jpg',
+        ime:'Huawei Matepad 11' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:45
+        },
+        Cijena: 78999,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/sound Joy.jpg",
+        ime:'Huawei Sound Joy' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:566
+        },
+        Cijena: 8990,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/matebookx.png",
+        ime:'Huawei Matebook X' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:98
+        },
+        Cijena: 134900,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/p60pro.jpg",
+        ime:'Huawei P60 Pro' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:155
+        },
+        Cijena: 119900,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/p40pro.jpg",
+        ime:'Huawei P40 Pro' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:22
+        },
+        Cijena: 56999,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/matebookd13.png",
+        ime:'Huawei Matebook D13' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:82
+        },
+        Cijena: 69900,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/p50pro.png",
+        ime:'Huawei P50 Pro' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:11
+        },
+        Cijena: 78849,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/matebookd14.jpg",
+        ime:'Huawei Matebook D14' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:23
+        },
+        Cijena: 134877,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/soundx (2).jpg",
+        ime:'Huawei Sound X' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:80
+        },
+        Cijena: 17800,
+
+    },
+    {
+        image:"/Ikone proizvoda lol/mateXS2.jpg",
+        ime:'Huawei Mate XS2' ,
+        ocjena :{
+            SlikaOcjene:'ocjenaArtikla',
+            brojOcjene:66
+        },
+        Cijena: 121900,
+
+    },
+
+]
