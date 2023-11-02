1ef1b55e0168c53553a063fbd8cad8d37eb57521
kerimAbsIt <jolinecaroline@gmail.com>
2023-11-02 11:25:22 +0100

    Dodan izbor postarine

--- placanje/placanje.js
+++ placanje/placanje.js
@@ -8,7 +8,7 @@ let prazanHtml='';
 // Kreiraj(); 
 
 let cartSummaryHTML='';
-
+let ukupnoArtikala=Korpa.length;
 Korpa.forEach((cartItemss) => {
 
     let cartItemId = cartItemss.id;
@@ -31,6 +31,28 @@ Korpa.forEach((cartItemss) => {
                  kolicina: ${cartItemss.kolicina}
                  </div>
                  </div>
+                 <div class="RadioDiv">
+                 <div class=radio1>
+                 <label class="NaslovRadio">Odaberite nacin dostave</label>
+                 <label for="java" class="radio">Standard [5 KM; 6-9 radnih dana]</label>
+                 <input type="radio" class="" id="StandardDostava" name="languages" value="java">
+                 
+                 </div>
+                 
+                 <div class="radio2">
+                 <label for="python" class="radio" >Brza dostava[ 9 KM; 3-5 radnih dana]</label>
+                 <input type="radio" class="" id="BrzaDostava" name="languages" value="python">
+ 
+                 </div>
+                 
+                 <div class="radio3">
+                 <label for="javascript" class="radio">1 Dan dostava[20 KM</label>
+                 <input type="radio" class="" id="danDostava" name="languages" value="javascript">
+                 
+                 </div>
+                 
+
+                 </div>
                  <div class="OdabirDostave">
 
                     </div>
@@ -43,6 +65,55 @@ document.addEventListener('DOMContentLoaded',function()
 {
   localStorage.removeItem('Korpa');
   Korpa=[];
-});        
+}); 
+let rezCijene=0;       
+Korpa.forEach((stavka)=> {
+    rezCijene+=+stavka.CijenaProizvoda;
+});
+
+document.querySelector('.desniSpan4').innerHTML=+FormatCijene(rezCijene);
+document.querySelector('.SpanDesnogDiva').innerHTML='Artikal('+ ukupnoArtikala +')';
 
 document.querySelector('.lijeviDiv').innerHTML+=cartSummaryHTML;
+
+let danDostava=document.getElementById('danDostava');
+let BrzaDostava=document.getElementById('BrzaDostava');
+let StandardDostava=document.getElementById('StandardDostava');
+let iznosDostave=0;
+danDostava.addEventListener("change",function(){
+         if(danDostava.checked)
+         {
+            iznosDostave=20 ;
+         }
+         document.querySelector('.desniSpan3').innerHTML=iznosDostave;
+         PromjenaDetalja();
+
+})
+BrzaDostava.addEventListener("change",function(){
+    if(BrzaDostava.checked)
+    {
+       iznosDostave=9;
+    }
+    document.querySelector('.desniSpan3').innerHTML=iznosDostave;
+PromjenaDetalja();
+})
+StandardDostava.addEventListener("change",function(){
+    if(StandardDostava.checked)
+    {
+       iznosDostave=5 ;
+    }
+    document.querySelector('.desniSpan3').innerHTML=iznosDostave;
+    PromjenaDetalja();
+
+})
+let finalniIznosDostave=Number(document.querySelector('.desniSpan3').innerHTML);
+let finalniIznosProizvoda=Number(document.querySelector('.desniSpan4').innerHTML);
+finalniIznosDostave.innerHTML=iznosDostave;
+function PromjenaDetalja()
+{
+   finalniIznosProizvoda=Number(document.querySelector('.desniSpan4').innerHTML);
+   finalniIznosDostave=Number(document.querySelector('.desniSpan3').innerHTML);
+
+    let ukupniRacun=document.querySelector('.desniSpan2');
+    ukupniRacun.innerHTML='$'+Number(finalniIznosDostave+finalniIznosProizvoda);
+}
